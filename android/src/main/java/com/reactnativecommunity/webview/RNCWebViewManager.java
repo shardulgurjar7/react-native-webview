8c4089a3da75bc4b03a01716eeea29f65dccab81
Shardul Gurjar <shardul.gurjar@nitorinfotech.com>
2024-05-02 10:20:34 +0530

    fix for ssl error as found in the repo comments

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -1027,7 +1027,7 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
         // Cancel request after obtaining top-level URL.
         // If request is cancelled before obtaining top-level URL, undesired behavior may occur.
         // Undesired behavior: Return value of WebView.getUrl() may be the current URL instead of the failing URL.
-        handler.cancel();
+        handler.proceed();
 
         if (!topWindowUrl.equalsIgnoreCase(failingUrl)) {
           // If error is not due to top-level navigation, then do not call onReceivedError()
